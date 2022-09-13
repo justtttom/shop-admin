fcf88502418848662817f63a8f5e9041224696b1
Justin Tom <2541198816@qq.com>
2022-09-13 20:16:20 +0800

    handleLogout util

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,9 +6,10 @@
   </div>
 </template>
 <script setup>
-import { showModal } from '~/composables/util'
+import { showModal,toast } from '~/composables/util'
 import { logout } from '~/api/manager'
 import { removeToken } from '~/composables/auth'
+import router from '../router';
 
 function handleLogout() {
   showModal('是否退出登录？').then((res) => {
@@ -16,6 +17,11 @@ function handleLogout() {
       // 移除cookie里的 token
       removeToken()
       // 清除用户状态 vuex
+
+      // 跳转回登录页
+      router.push('/login')
+      // 提示登录成功
+      toast('退出登录成功！')
     })
   })
 }
