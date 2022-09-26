20848e47eac587eecf0bc4f32a3022d3eef2f1a9
Justin Tom <2541198816@qq.com>
2022-09-26 22:27:57 +0800

    goodnight

--- src/layouts/components/FMenu.vue
+++ src/layouts/components/FMenu.vue
@@ -1,26 +1,39 @@
 <template>
-  <div class="f-menu" :style="{width:$store.state.asideWidth}">
-    <el-menu default-active="2" class="border-0" @select="handleSelect">
-      <template v-for="(item,index) in asideMenus" :key="index">
-        <el-sub-menu v-if="item.child && item.child.length > 0" :index="item.name">
+  <div class="f-menu" :style="{ width: $store.state.asideWidth }">
+    <el-menu
+      :collapse="isCollapse"
+      default-active="2"
+      class="border-0"
+      @select="handleSelect"
+      :collapse-transition="false"
+    >
+      <template v-for="(item, index) in asideMenus" :key="index">
+        <el-sub-menu
+          v-if="item.child && item.child.length > 0"
+          :index="item.name"
+        >
           <template #title>
             <el-icon>
               <component :is="item.icon"></component>
             </el-icon>
-            <span>{{item.name}}</span>
+            <span>{{ item.name }}</span>
           </template>
-          <el-menu-item v-for="(item2,index2) in item.child" :key="index2" :index="item2.frontpath">
+          <el-menu-item
+            v-for="(item2, index2) in item.child"
+            :key="index2"
+            :index="item2.frontpath"
+          >
             <el-icon>
               <component :is="item2.icon"></component>
             </el-icon>
-            <span>{{item2.name}}</span>
+            <span>{{ item2.name }}</span>
           </el-menu-item>
         </el-sub-menu>
         <el-menu-item v-else :index="item.frontpath">
           <el-icon>
             <component :is="item.icon"></component>
           </el-icon>
-          <span>{{item.name}}</span>
+          <span>{{ item.name }}</span>
         </el-menu-item>
       </template>
     </el-menu>
@@ -28,9 +41,15 @@
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex';
 
 const router = useRouter()
+const store = useStore()
+
+// 菜单是否折叠
+const isCollapse = computed(() => !(store.state.asideWidth == '250px'))
 
 const asideMenus = [
   {
@@ -57,7 +76,7 @@ const asideMenus = [
   }
 ]
 
-const handleSelect = (e)=>{
+const handleSelect = (e) => {
   router.push(e)
 }
 </script>
