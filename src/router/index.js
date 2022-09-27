71eb74a6915f3c2366415e21511668cd5240cfe6
Justin Tom <2541198816@qq.com>
2022-09-27 23:08:27 +0800

    goodnight

--- src/router/index.js
+++ src/router/index.js
@@ -7,55 +7,75 @@ import GoodList from '~/pages/goods/list.vue'
 import CategoryList from '~/pages/category/list.vue'
 import Admin from '~/layouts/admin.vue'
 
+// const routes = [
+//   {
+//     path: "/",
+//     component:Admin,
+
+//     // 子路由
+//     children:[
+//       {
+//         path: '/',
+//         component:Index,
+//         meta:{
+//           title: '首页'
+//         }
+//       },
+//       {
+//         path: "/goods/list",
+//         component: GoodList,
+//         meta: {
+//           title: '商品管理页'
+//         }
+//       },{
+//         path: '/category/list',
+//         component:CategoryList,
+//         meta: {
+//           title: '分类管理页'
+//         }},
+//     ]
+//   }, {
+//   path: '/login',
+//   component: Login,
+//   meta: {
+//     title: '登录页'
+//   }
+// },
+
+// {
+//   path: '/:pathMatch(.*)*',
+//   name: 'NotFound',
+//   component: NotFound,
+//   meta: {
+//     title: '你的页面走丢了'
+//   }
+// }
+// ]
+
+//默认路由，所有用户共享
 const routes = [
   {
     path: "/",
-    component:Admin,
-    // meta:{
-    //   title:'管理页面'
-    // },
-    // 子路由
-    children:[
-      {
-        path: '/',
-        component:Index,
-        meta:{
-          title: '首页'
-        }
-      },
-      {
-        path: "/goods/list",
-        component: GoodList,
-        meta: {
-          title: '商品管理页'
-        }
-      },{
-        path: '/category/list',
-        component:CategoryList,
-        meta: {
-          title: '分类管理页'
-        }},
-    ]
+    name:'admin',
+    component: Admin,
   }, {
-  path: '/login',
-  component: Login,
-  meta: {
-    title: '登录页'
+    path: '/login',
+    component: Login,
+    meta: {
+      title: '登录页'
+    }
+  },{
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound,
+    meta: {
+      title: '你的页面走丢了'
+    }
   }
-},
-
-{
-  path: '/:pathMatch(.*)*',
-  name: 'NotFound',
-  component: NotFound,
-  meta: {
-    title: '你的页面走丢了'
-  }
-}
 ]
 
 const router = createRouter({
-  history:createWebHashHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
