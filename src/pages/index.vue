c95d8a8c30efc3ee4d7b86d0043e21cb672e5bc1
Justin Tom <2541198816@qq.com>
2022-10-11 22:27:40 +0800

    index.vue style change

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
     <el-row :gutter="20">
-      <el-col :span="6" :offset="0" v-for="(item,index) in panels" :key="index">
-        <el-card shadow="hover">
+      <el-col
+        :span="6"
+        :offset="0"
+        v-for="(item, index) in panels"
+        :key="index"
+      >
+        <el-card shadow="hover" class="border-0">
           <template #header>
-            <div>
-              <span>{{item.title}}</span>
+            <div class="flex justify-between">
+              <span class="text-sm">{{ item.title }}</span>
               <el-tag :type="item.unitColor" effect="plain">
-                {{ item.value }}
+                {{ item.unit }}
               </el-tag>
             </div>
           </template>
-          <!-- card body -->
+          <span class="text-3xl font-bold text-gray-600">
+            {{ item.value }}
+          </span>
+          <el-divider />
+          <div class="flex justify-between text-sm text-gray-600">
+            <span>{{ item.subTitle }}:</span>
+            <span>{{ item.subValue }}</span>
+          </div>
         </el-card>
       </el-col>
     </el-row>
@@ -22,12 +34,10 @@ import { ref } from 'vue'
 import { getStatistics1 } from '~/api/index.js'
 
 const panels = ref([])
-getStatistics1().then(res => {
+getStatistics1().then((res) => {
   panels.value = res.panels
-  console.log(panels.value);
+  console.log(panels.value)
 })
 </script>
 
-<style>
-
-</style>
+<style></style>
