2f908dbdc1012cb377e667e7500581553308f53a
Justin Tom <2541198816@qq.com>
2022-10-11 21:33:05 +0800

    get getStatistics1 data

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,5 +4,12 @@
   </div>
 </template>
 <script setup>
+import { ref } from 'vue'
 import { getStatistics1 } from '~/api/index.js'
+
+const panels = ref([])
+getStatistics1().then(res => {
+  panels.value = res.panels
+  console.log(panels.value);
+})
 </script>
