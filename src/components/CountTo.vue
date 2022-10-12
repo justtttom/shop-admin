7868422d304c343df99f1d6992dfbff1b528879c
Justin Tom <2541198816@qq.com>
2022-10-12 20:23:13 +0800

    add props in CountTo.vue

--- src/components/CountTo.vue
+++ src/components/CountTo.vue
@@ -6,8 +6,16 @@
 import {reactive} from 'vue'
 import gsap from 'gsap'
 
+const props = defineProps({
+  value:{
+    type:Number,
+    default:0
+  }
+})
+
 const d = reactive({
   num:0,
 })
+
 
 </script>
