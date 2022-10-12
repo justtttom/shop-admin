2163f2463c6d0b42ce2324c5962068c7b1c2b81a
Justin Tom <2541198816@qq.com>
2022-10-12 22:14:42 +0800

    goodnight

--- src/components/IndexNavs.vue
+++ src/components/IndexNavs.vue
@@ -3,6 +3,7 @@
     <el-col :span="3" :offset="0">
     <el-card shadow="hover" >
       <div class="flex justify-center items-center cursor-pointer">
+        <el-icon><Odometer /></el-icon>
       </div>
     </el-card>
     </el-col>
