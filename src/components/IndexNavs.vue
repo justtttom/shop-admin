88ee9caf9a3f20c8f513a6aaf6877c7e84db37ab
Justin Tom <2541198816@qq.com>
2022-10-12 21:54:51 +0800

    add indexNav

---
+++ src/components/IndexNavs.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="3" :offset="0">
+    <el-card shadow="hover" >
+      <div class="flex justify-center items-center cursor-pointer">
+      </div>
+    </el-card>
+    </el-col>
+  </el-row>
+  
+</template>
