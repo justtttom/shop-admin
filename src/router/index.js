f7ab4dbfac85353999dbe762d82684817d0aa761
Justin Tom <2541198816@qq.com>
2022-10-13 22:53:20 +0800

    add route about pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
+import Admin from '~/layouts/admin.vue'
 import Index from '~/pages/index.vue'
 import NotFound from '~/pages/404.vue'
 import Login from '~/pages/login.vue'
 import GoodList from '~/pages/goods/list.vue'
 import CategoryList from '~/pages/category/list.vue'
-import Admin from '~/layouts/admin.vue'
+import UserList from '~/pages/user/list.vue'
+import OrderList from '~/pages/order/list.vue'
+import CommentList from '~/pages/comment/list.vue'
+import ImageList from '~/pages/image/list.vue'
+import NoticeList from '~/pages/notice/list.vue'
+import SettingBase from '~/pages/setting/base.vue'
+import CouponList from '~/pages/coupon/list.vue'
+
 
 //默认路由，所有用户共享
 const routes = [
@@ -46,12 +54,61 @@ const asyncRoutes = [
     meta: {
       title: '商品管理页'
     }
-  }, {
+  },{
     path: '/category/list',
     name:'/category/list',
     component: CategoryList,
     meta: {
       title: '分类管理页'
+    }
+  }, {
+    path: '/user/list',
+    name:'/user/list',
+    component: UserList,
+    meta: {
+      title: '用户列表'
+    }
+  }, {
+    path: '/order/list',
+    name:'/order/list',
+    component: OrderList,
+    meta: {
+      title: '订单列表'
+    }
+  }, {
+    path: '/comment/list',
+    name:'/comment/list',
+    component: CommentList,
+    meta: {
+      title: '评价列表'
+    }
+  }, {
+    path: '/image/list',
+    name:'/image/list',
+    component: ImageList,
+    meta: {
+      title: '图库列表'
+    }
+  }, {
+    path: '/notice/list',
+    name:'/notice/list',
+    component: NoticeList,
+    meta: {
+      title: '公告列表'
+    }
+  }, {
+    path: '/setting/base',
+    name:'/setting/base',
+    component: SettingBase,
+    meta: {
+      title: '配置列表'
+    }
+  }, {
+    path: '/coupon/list',
+    name:'/coupon/list',
+    component: CouponList,
+    meta: {
+      title: '优惠券列表'
     }
   },
 ]
