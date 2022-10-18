0a360174bd7e44df38ef0433d296a7ed02258527
Justin Tom <2541198816@qq.com>
2022-10-18 19:54:58 +0800

    tuesday work

---
+++ src/components/Indexcart.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-card shadow="never">
+    <template #header>
+      <div class="flex justify-between">
+        <span class="text-sm">{{title }}</span>
+        <el-tag :type="danger" effect="plain">
+          {{tip}}
+        </el-tag>
+      </div>
+    </template>
+  </el-card>
+</template>
+
+<script setup>
+  defineProps({
+    title:string,
+    tip:string,
+  })
+</script>
