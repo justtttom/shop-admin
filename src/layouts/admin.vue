107d4e80d7d56c8a4a8d40472fcd4ef3d5d5a5cc
Justin Tom <2541198816@qq.com>
2022-10-19 20:38:43 +0800

    change the style of the admin.vue

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,61 +1,51 @@
 <template>
-  <div class="common-layout">
     <el-container>
-      <el-header>
-        <f-header />
-      </el-header>
-      <el-container>
-        <el-aside :width="$store.state.asideWidth">
-          <f-menu />
-        </el-aside>
-        <el-main class="bg-gray-100">
-          <f-tag-list />
-          <router-view v-slot="{Component}">
-            <Transition name="fade">
-              <KeepAlive :max="10">
-                <component :is="Component"></component>
-              </KeepAlive>
-            </Transition>
-          </router-view>
-        </el-main>
-      </el-container>
+        <el-header>
+            <f-header/>
+        </el-header>
+        <el-container>
+            <el-aside :width="$store.state.asideWidth">
+                <f-menu></f-menu>
+            </el-aside>
+            <el-main class="bg-gray-50">
+                <f-tag-list/>
+                <router-view v-slot="{ Component }">
+                    <transition name="fade">
+                        <keep-alive :max="10">
+                            <component :is="Component"></component>
+                        </keep-alive>
+                    </transition>
+                </router-view>
+            </el-main>
+        </el-container>
     </el-container>
-  </div>
 </template>
-
 <script setup>
-import FHeader from './components/FHeader.vue'
-import FMenu from './components/FMenu.vue'
-import FTagList from './components/FTagList.vue'
+import FHeader from './components/FHeader.vue';
+import FMenu from './components/FMenu.vue';
+import FTagList from './components/FTagList.vue';
 </script>
-
 <style>
-.el-aside {
-  transition: all 0.2s;
-} 
-
-.fade-enter-from {
-  opacity: 0;
+.el-aside{
+    transition: all 0.2s;
 }
-
-.fade-enter-to {
-  opacity: 1;
+.fade-enter-from{
+    opacity: 0;
 }
-
-.fade-leave-from {
-  opacity: 1;
+.fade-enter-to{
+    opacity: 1;
 }
-
-.fade-leave-to {
-  opacity: 0;
+.fade-leave-from{
+    opacity: 1;
 }
-
+.fade-leave-to{
+    opacity: 0;
+}
 .fade-enter-active,
-.fade-leave-active {
-  transition: all 0.3s;
+.fade-leave-active{
+    transition: all 0.3s;
 }
-
-.fade-enter-active {
-  transition-delay: 0.3s;
+.fade-enter-active{
+    transition-delay: 0.3s;
 }
 </style>
--- src/layouts/components/FTagList.vue
+++ src/layouts/components/FTagList.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="f-tag-list" :style="{ left: $store.state.asideWidth }">
-    <el-tabs v-model="activeTab" type="card" @tab-remove="removeTab" style="min-width: 100px;" @tab-change="changeTab">
-      <el-tab-pane :closable="item.path != '/'" v-for="item in tabList" :key="item.path" :label="item.title"
-        :name="item.path">
-      </el-tab-pane>
+    <el-tabs
+      v-model="activeTab"
+      type="card"
+      class="flex-1"
+      @tab-remove="removeTab"
+      style="min-width: 100px"
+      @tab-change="changeTab"
+    >
+      <el-tab-pane
+        :closable="item.path != '/'"
+        v-for="item in tabList"
+        :key="item.path"
+        :label="item.title"
+        :name="item.path"
+      ></el-tab-pane>
     </el-tabs>
     <span class="tag-btn">
       <el-dropdown @command="handleClose">
@@ -21,50 +32,44 @@
       </el-dropdown>
     </span>
   </div>
-  <div style="height: 44px;"></div>
+  <div style="height: 44px"></div>
 </template>
 <script setup>
-import { useTabList } from '~/composables/useTabList'
-const { activeTab,tabList,changeTab,removeTab,handleClose } = useTabList()
+import { useTabList } from "~/composables/useTabList.js";
+const { activeTab, tabList, changeTab, removeTab, handleClose } = useTabList();
 </script>
 <style scoped>
 .f-tag-list {
-  @apply fixed flex items-center px-2 border-0;
+  @apply fixed bg-gray-100 flex items-center px-2;
   top: 64px;
   right: 0;
   height: 44px;
   z-index: 100;
 }
-
 .tag-btn {
   @apply bg-white rounded ml-auto flex items-center justify-center px-2;
-  width: 32px;
   height: 32px;
 }
-
 :deep(.el-tabs__header) {
-  border: 0!important;
-  @apply mb-0;
+  border: 0 !important;
+  @apply mb-0 mt-2;
 }
-
 :deep(.el-tabs__nav) {
   border: 0 !important;
 }
-
 :deep(.el-tabs__item) {
   border: 0 !important;
   height: 32px;
   line-height: 32px;
   @apply bg-white mx-1 rounded;
 }
-
 :deep(.el-tabs__nav-next),
 :deep(.el-tabs__nav-prev) {
   line-height: 32px;
+  height: 32px;
 }
-
 :deep(.is-disabled) {
   cursor: not-allowed;
-  @apply text-red-300;
+  @apply text-gray-300;
 }
 </style>
