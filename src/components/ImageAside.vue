4c682461511d267830e04fbc44e1318db23e0206
Justin Tom <2541198816@qq.com>
2022-10-20 21:28:03 +0800

    create ImageAside and ImageMain unit

---
+++ src/components/ImageAside.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-aside width="220px" class="image-aside">
+    <div class="top">
+      <div v-for="i in 100" :key="i">{{ i }}</div>
+    </div>
+    <div class="bottom">分页区域</div>
+  </el-aside>
+</template>
+<script setup >
+  
+</script>
+<style scoped>
+  .image-aside {
+  border-right: 1px solid #eee;
+  position: relative;
+}
+.image-aside .top{
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 50px;
+  overflow-y: auto;
+}
+.image-aside .bottom{
+  @apply flex items-center justify-center;
+  position: absolute;
+  bottom: 0;
+  height: 50px;
+  right: 0;
+  left: 0;
+}
+</style>
--- src/pages/image/list.vue
+++ src/pages/image/list.vue
@@ -1,24 +1,17 @@
 <template>
   <el-container class="bg-white rounded" :style="{ height: h + 'px' }">
-    <el-header class="image-header bg-green-400">Header</el-header>
+    <el-header class="image-header">Header</el-header>
     <el-container>
-      <el-aside width="220px" class="image-aside bg-orange-400">
-        <div class="top">
-          <div v-for="i in 100" :key="i">{{ i }}</div>
-        </div>
-        <div class="bottom">分页区域</div>
-      </el-aside>
-      <el-main class="image-main">
-        <div class="top">
-          <div v-for="i in 100" :key="i">{{ i }}</div>
-        </div>
-        <div class="bottom">分页区域</div>
-      </el-main>
+      <ImageAside/>
+      <ImageMain/>
     </el-container>
   </el-container>
 </template>
 
 <script setup>
+import ImageAside from '~/components/ImageAside.vue'
+import ImageMain from '~/components/ImageMain.vue'
+
 const windowHeight = window.innerHeight || document.body.clientHeight;
 const h = windowHeight - 60 - 44 - 40;
 </script>
@@ -27,29 +20,5 @@ const h = windowHeight - 60 - 44 - 40;
 .image-header {
   border-bottom: 1px solid #eee;
   @apply flex items-center;
-}
-.image-aside {
-  border-right: 1px solid #eee;
-  position: relative;
-}
-.image-main {
-  position: relative;
-}
-.image-aside .top,.image-main .top{
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 50px;
-  overflow-y: auto;
-}
-
-.image-aside .bottom,.image-main .bottom{
-  @apply flex items-center justify-center;
-  position: absolute;
-  bottom: 0;
-  height: 50px;
-  right: 0;
-  left: 0;
 }
 </style>
