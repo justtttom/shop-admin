3060b3bac649a9da1faebbe13dbe37ad89df57a1
Justin Tom <2541198816@qq.com>
2022-10-20 21:08:14 +0800

    scrollbar style change

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 </template>
 
 <style>
-.body{
-  @apply bg-gray-100
+.body {
+  @apply bg-gray-100;
 }
 #nprogress .bar {
   background: #f0eeec !important;
@@ -15,5 +15,22 @@
 #nprogress .spinner-icon {
   border-top-color: #7d7bf2e6 !important;
   border-left-color: #7d7bf2e6 !important;
+}
+::-webkit-scrollbar {
+  width: 5px;
+  height: 6px;
+}
+::-webkit-scrollbar-corner {
+  display: block;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 8px;
+  background-color: rgba(0, 0, 0, 0.2);
+}
+::-webkit-scrollbar-thumb,
+::-webkit-scrollbar-track {
+  border-right-color: transparent;
+  border-left-color: transparent;
+  background-color: rgba(0, 0, 0, 0.1);
 }
 </style>
