b1e5a016baccadead5bdbddc26eaad60ce538d8f
Justin Tom <2541198816@qq.com>
2022-10-20 20:15:30 +0800

    change the image page style

--- src/pages/image/list.vue
+++ src/pages/image/list.vue
@@ -1,9 +1,14 @@
 <template>
-  <el-container class="bg-white rounded">
-    <el-header>Header</el-header>
+  <el-container class="bg-white rounded" :style="{ height: h + 'px' }">
+    <el-header class="image-header">Header</el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
-      <el-main>Main</el-main>
+      <el-aside width="220px" class="image-aside">Aside</el-aside>
+      <el-main class="image-main">Main</el-main>
     </el-container>
   </el-container>
 </template>
+
+<script setup>
+const windowHeight = window.innerHeight || document.body.clientHeight;
+const h = windowHeight - 60 - 44 - 40;
+</script>
