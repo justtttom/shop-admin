482262df91a4880281b41517a4133fe794e1c8d1
Justin Tom <2541198816@qq.com>
2022-10-21 21:21:14 +0800

    add  aside list active and hover status

--- src/components/ImageAside.vue
+++ src/components/ImageAside.vue
@@ -10,6 +10,16 @@
           <el-icon :size="12"><Close /></el-icon>
         </el-button>
       </div>
+
+       <div class="aside-list active">
+        <span class="truncate">分类标题</span>
+        <el-button class="ml-auto px-1" text type="primary" size="small">
+          <el-icon :size="12"><Edit /></el-icon>
+        </el-button>
+        <el-button text type="primary" size="small">
+          <el-icon :size="12"><Close /></el-icon>
+        </el-button>
+      </div>
     </div>
     <div class="bottom">分页区域</div>
   </el-aside>
@@ -41,7 +51,7 @@
   cursor: pointer;
   @apply flex items-center p-3 text-sm text-gray-600;
 }
-.aside-list:hover{
+.aside-list:hover,.active{
   @apply bg-blue-50;
 }
 </style>
