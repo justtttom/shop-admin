c9aada408c7792d2484beac5f801d97d64c91d1f
Justin Tom <2541198816@qq.com>
2022-10-21 21:25:49 +0800

    create AsideList,vue

---
+++ src/components/AsideList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="aside-list">
+      <span class="truncate">分类标题</span>
+      <el-button class="ml-auto px-1" text type="primary" size="small">
+        <el-icon :size="12"><Edit /></el-icon>
+      </el-button>
+      <el-button text type="primary" size="small">
+        <el-icon :size="12"><Close /></el-icon>
+      </el-button>
+    </div>
+    <div class="aside-list active">
+        <span class="truncate">分类标题</span>
+        <el-button class="ml-auto px-1" text type="primary" size="small">
+          <el-icon :size="12"><Edit /></el-icon>
+        </el-button>
+        <el-button text type="primary" size="small">
+          <el-icon :size="12"><Close /></el-icon>
+        </el-button>
+      </div>
+</template>
+
+<style scoped>
+.aside-list {
+  border-bottom: 1px solid #f4f4f4;
+  cursor: pointer;
+  @apply flex items-center p-3 text-sm text-gray-600;
+}
+.aside-list:hover,
+.active {
+  @apply bg-blue-50;
+}
+</style>
--- src/components/ImageAside.vue
+++ src/components/ImageAside.vue
@@ -1,30 +1,14 @@
 <template>
   <el-aside width="220px" class="image-aside">
     <div class="top">
-      <div class="aside-list">
-        <span class="truncate">分类标题</span>
-        <el-button class="ml-auto px-1" text type="primary" size="small">
-          <el-icon :size="12"><Edit /></el-icon>
-        </el-button>
-        <el-button text type="primary" size="small">
-          <el-icon :size="12"><Close /></el-icon>
-        </el-button>
-      </div>
-
-       <div class="aside-list active">
-        <span class="truncate">分类标题</span>
-        <el-button class="ml-auto px-1" text type="primary" size="small">
-          <el-icon :size="12"><Edit /></el-icon>
-        </el-button>
-        <el-button text type="primary" size="small">
-          <el-icon :size="12"><Close /></el-icon>
-        </el-button>
-      </div>
+      <AsideList/>
     </div>
     <div class="bottom">分页区域</div>
   </el-aside>
 </template>
-
+<script setup>
+  import AsideList from './AsideList.vue'
+</script>
 <style scoped>
 .image-aside {
   border-right: 1px solid #eee;
@@ -46,12 +30,5 @@
   right: 0;
   left: 0;
 }
-.aside-list{
-  border-bottom: 1px solid #f4f4f4;
-  cursor: pointer;
-  @apply flex items-center p-3 text-sm text-gray-600;
-}
-.aside-list:hover,.active{
-  @apply bg-blue-50;
-}
+
 </style>
