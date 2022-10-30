92d0a29fed15b882d49e44f920bf733de17cb3c5
Justin Tom <2541198816@qq.com>
2022-10-30 21:54:11 +0800

    modification the ImageMain.vue

--- src/components/ImageMain.vue
+++ src/components/ImageMain.vue
@@ -1,12 +1,41 @@
 <template>
-  <el-main class="image-main">
+  <el-main class="image-main" v-loading="">
     <div class="top">
       <div v-for="i in 10" :key="i">{{ i }}</div>
     </div>
-    <div class="bottom">分页区域</div>
+    <div class="bottom">
+      <el-pagination background layout="prev, next" :total="total" :current-page="currentPage" :page-size="limit"
+        @current-change="getData" />
+    </div>
   </el-main>
 </template>
+<script setup >
+  import { getImageList } from '~/api/image.js';
+  import { ref } from 'vue';
 
+  // 分页
+const currentPage = ref(1)
+const total = ref(0)
+const limit = ref(10)
+const list = ref([])
+const loading = ref(false)
+
+// 获取数据
+function getData(p = null) {
+  if (typeof p == 'number') {
+    currentPage.value = p
+  }
+  loading.value = true;
+  getImageList(currentPage.value)
+    .then((res) => {
+      total.value = res.totalCount
+      list.value = res.list
+    })
+    .finally(() => [(loading.value = false)]);
+}
+// getData();
+
+</script>
 <style scoped>
 .image-main {
   position: relative;
