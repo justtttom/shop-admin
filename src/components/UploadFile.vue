6d7a68de8b20d6d37e4b9225a58f1a3e5a06cd56
Justin Tom <2541198816@qq.com>
2022-11-07 19:45:28 +0800

    change the action address  in UploadFile

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -2,7 +2,7 @@
   <el-upload
     class="upload-demo"
     drag
-    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+    action="/api/admin/image/upload"
     multiple
   >
     <el-icon class="el-icon--upload"><upload-filled /></el-icon>
