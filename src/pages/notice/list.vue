d676161e7e3d9739474144b021612de367647823
Justin Tom <2541198816@qq.com>
2022-11-08 19:46:30 +0800

    notice.page change

--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    公告列表
-  </div>
+  <el-card shadow="hover" class="border-0">
+    <div class="flex justify-between items-center mb-4">
+      <el-button type="primary" size="small" @click="">新增</el-button>
+      
+    </div>
+  </el-card>
+  
 </template>
