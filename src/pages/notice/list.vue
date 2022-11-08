1ac26656cc97235b16da2202f1840b69dbcada06
Justin Tom <2541198816@qq.com>
2022-11-08 20:03:13 +0800

    rest

--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -2,17 +2,20 @@
   <el-card shadow="hover" class="border-0">
     <div class="flex justify-between items-center mb-4">
       <el-button type="primary" size="small" @click="">新增</el-button>
-      <el-tooltip
-        class="box-item"
-        effect="dark"
-        content="点击刷新"
-        placement="top-start"
-      >
+      <el-tooltip effect="dark" content="刷新数据" placement="top-start">
         <el-button text>
-          <el-icon><RefreshRight /></el-icon>
+          <el-icon :size="20"><Refresh /></el-icon>
         </el-button>
       </el-tooltip>
     </div>
+    <el-table :data="tableData" stripe style="width: 100%;">
+      <el-table-column prop="date" label="公告标题" width="180" />
+      <el-table-column prop="name" label="发布时间" width="180" />
+      <el-table-column prop="address" label="操作" />
+    </el-table>
   </el-card>
-  
 </template>
+
+<script setup>
+  
+</script>
