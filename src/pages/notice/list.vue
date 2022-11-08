f9d01811bf1d4b0c269f95e636d8ce569a40d3ff
Justin Tom <2541198816@qq.com>
2022-11-08 19:55:38 +0800

    import el-tooltip  in  notice.page

--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -2,7 +2,16 @@
   <el-card shadow="hover" class="border-0">
     <div class="flex justify-between items-center mb-4">
       <el-button type="primary" size="small" @click="">新增</el-button>
-      
+      <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="点击刷新"
+        placement="top-start"
+      >
+        <el-button text>
+          <el-icon><RefreshRight /></el-icon>
+        </el-button>
+      </el-tooltip>
     </div>
   </el-card>
   
