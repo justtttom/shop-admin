a83d8a485e9691e97a344d3dae0454c4f72eb568
Justin Tom <2541198816@qq.com>
2022-11-14 22:25:20 +0800

    Goodnight

---
+++ src/pages/manager/list.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-card shadow="never" class="border-0">
+    <template #header>
+      <div>
+        关键词
+        <el-input size="small" width="20px" placeholder="管理员昵称" />
+      </div>
+    </template>
+    <!-- card body -->
+  </el-card>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import ImageList from '~/pages/image/list.vue'
 import NoticeList from '~/pages/notice/list.vue'
 import SettingBase from '~/pages/setting/base.vue'
 import CouponList from '~/pages/coupon/list.vue'
+import ManagerList from '~/pages/manager/list.vue'
 
 
 //默认路由，所有用户共享
@@ -109,6 +110,13 @@ const asyncRoutes = [
     component: CouponList,
     meta: {
       title: '优惠券列表'
+    }
+  },{
+    path: '/manager/list',
+    name:'/manger/list',
+    component: ManagerList,
+    meta: {
+      title: '管理员页面'
     }
   },
 ]
