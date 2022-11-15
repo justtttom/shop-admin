d7873618a6e6a110f3d7c8811742f82a944a7ca4
Justin Tom <2541198816@qq.com>
2022-11-15 21:37:22 +0800

    goodnight

--- src/pages/manager/list.vue
+++ src/pages/manager/list.vue
@@ -1,72 +1,54 @@
 <template>
   <el-card shadow="never" class="border-0">
     <div class="flex justify-between items-center mb-4">
-      <el-button type="primary" size="small" @click="handleCreate"
-        >新增</el-button
-      >
+      <el-button type="primary" size="small" @click="handleCreate">新增</el-button>
       <el-tooltip effect="dark" content="刷新数据" placement="top">
         <el-button text @click="getData">
-          <el-icon :size="20"><Refresh /></el-icon>
+          <el-icon :size="20">
+            <Refresh />
+          </el-icon>
         </el-button>
       </el-tooltip>
     </div>
-    <el-table :data="tableData" stripe style="width: 100%;" v-loading="loading">
-      <el-table-column prop="username" label="管理员" />
-      <el-table-column prop="create_time" label="所属管理员" width="300" />
-      <el-table-column prop="" label="状态" width="300" />
-      <el-table-column label="操作" width="180" align="center">
+    <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
+      <el-table-column label="管理员" width="200">
+        <template #default="{ row }">
+          <div class="flex items-center">
+            <el-avatar :size="30" src="row.avatar">
+              <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png" />
+            </el-avatar>
+            <div class="ml-3">
+              <h6>{{row.username}}</h6>
+              <small>ID:{{row.id}}</small>
+            </div>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="create_time" label="所属管理员" width="160" />
+      <el-table-column prop="" label="状态" width="200" />
+      <el-table-column label="操作" width="380" align="center">
         <template #default="scope">
-          <el-button
-            size="small"
-            type="primary"
-            @click="handleEdit(scope.row)"
-            text
-            >修改</el-button
-          >
-          <el-popconfirm
-            title="是否要删除该公告？"
-            confirm-button-text="确定"
-            cancel-button-text="取消"
-            width="20"
-            @confirm="handleDelete(scope.row.id)"
-          >
+          <el-button size="small" type="primary" @click="handleEdit(scope.row)" text>修改</el-button>
+          <el-popconfirm title="是否要删除该公告？" confirm-button-text="确定" cancel-button-text="取消" width="20"
+            @confirm="handleDelete(scope.row.id)">
             <template #reference>
-              <el-button size="small" type="primary" @click="" text
-                >删除</el-button
-              >
+              <el-button size="small" type="primary" @click="" text>删除</el-button>
             </template>
           </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
     <div class="flex items-center justify-center mt-5">
-      <el-pagination
-        background
-        layout="prev, pager,next"
-        :total="total"
-        :current-page="currentPage"
-        :page-size="limit"
-        @current-change="getData"
-      />
+      <el-pagination background layout="prev, pager,next" :total="total" :current-page="currentPage" :page-size="limit"
+        @current-change="getData" />
     </div>
     <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
-      <el-form
-        :model="form"
-        ref="formRef"
-        :rules="rules"
-        label-width="80px"
-        :inline="false"
-      >
+      <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
         <el-form-item label="公告标题" prop="title">
           <el-input v-model="form.username" placeholder="公告标题"></el-input>
         </el-form-item>
         <el-form-item label="公告内容" prop="content">
-          <el-input
-            v-model="form.content"
-            placeholder="公告内容"
-            type="textarea"
-            :rows="5"
-          ></el-input>
+          <el-input v-model="form.content" placeholder="公告内容" type="textarea" :rows="5"></el-input>
         </el-form-item>
       </el-form>
     </FormDrawer>
