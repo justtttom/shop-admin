ca65c2dea034ee24d78583396ec12090cb6d993d
Justin Tom <2541198816@qq.com>
2022-11-15 20:00:08 +0800

    rest

--- src/api/manager.js
+++ src/api/manager.js
@@ -1,20 +1,37 @@
-import axios from "~/axios";
+import axios from '~/axios'
 
-export function login(username,password) {
-  return axios.post("/admin/login",{
+export function login(username, password) {
+  return axios.post('/admin/login', {
     username,
     password
   })
 }
 
 export function getInfo() {
-  return axios.post("/admin/getinfo")
+  return axios.post('/admin/getinfo')
 }
 
 export function logout() {
-  return axios.post("/admin/logout")
+  return axios.post('/admin/logout')
 }
 
 export function updatepassword(data) {
-  return axios.post('/admin/updatepassword',data)
+  return axios.post('/admin/updatepassword', data)
+}
+
+export function getManagerList(
+  page,
+  query = {
+    limit: 10,
+    keyword: 'ceshi'
+  }
+) {
+  let q = []
+  for (const key in query) {
+    if (query[key]) {
+      q.push(`${key}=${encodeURIComponent(query[key])}`)
+    }
+  }
+  let r = q.join('&')
+  return axios.post(`admin/manager/${page}?limit=10&keyword=ceshi`, data)
 }
--- src/pages/manager/list.vue
+++ src/pages/manager/list.vue
@@ -1,11 +1,183 @@
 <template>
   <el-card shadow="never" class="border-0">
-    <template #header>
-      <div>
-        关键词
-        <el-input size="small" width="20px" placeholder="管理员昵称" />
-      </div>
-    </template>
-    <!-- card body -->
+    <div class="flex justify-between items-center mb-4">
+      <el-button type="primary" size="small" @click="handleCreate"
+        >新增</el-button
+      >
+      <el-tooltip effect="dark" content="刷新数据" placement="top">
+        <el-button text @click="getData">
+          <el-icon :size="20"><Refresh /></el-icon>
+        </el-button>
+      </el-tooltip>
+    </div>
+    <el-table :data="tableData" stripe style="width: 100%;" v-loading="loading">
+      <el-table-column prop="title" label="公告标题" />
+      <el-table-column prop="create_time" label="发布时间" width="380" />
+      <el-table-column label="操作" width="180" align="center">
+        <template #default="scope">
+          <el-button
+            size="small"
+            type="primary"
+            @click="handleEdit(scope.row)"
+            text
+            >修改</el-button
+          >
+          <el-popconfirm
+            title="是否要删除该公告？"
+            confirm-button-text="确定"
+            cancel-button-text="取消"
+            width="20"
+            @confirm="handleDelete(scope.row.id)"
+          >
+            <template #reference>
+              <el-button size="small" type="primary" @click="" text
+                >删除</el-button
+              >
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="flex items-center justify-center mt-5">
+      <el-pagination
+        background
+        layout="prev, pager,next"
+        :total="total"
+        :current-page="currentPage"
+        :page-size="limit"
+        @current-change="getData"
+      />
+    </div>
+    <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
+      <el-form
+        :model="form"
+        ref="formRef"
+        :rules="rules"
+        label-width="80px"
+        :inline="false"
+      >
+        <el-form-item label="公告标题" prop="title">
+          <el-input v-model="form.title" placeholder="公告标题"></el-input>
+        </el-form-item>
+        <el-form-item label="公告内容" prop="content">
+          <el-input
+            v-model="form.content"
+            placeholder="公告内容"
+            type="textarea"
+            :rows="5"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+    </FormDrawer>
   </el-card>
 </template>
+
+<script setup>
+import { ref, reactive, computed } from 'vue'
+import FormDrawer from '~/components/FormDrawer.vue'
+import {
+  getManagerList,
+} from '~/api/mannager.js'
+import { toast } from '~/composables/util.js'
+
+const tableData = ref([])
+const loading = ref(false)
+
+// 分页
+const currentPage = ref(1)
+const total = ref(0)
+const limit = ref(10)
+
+// 获取数据
+function getData(p = null) {
+  if (typeof p == 'number') {
+    currentPage.value = p
+  }
+  loading.value = true
+  getManagerList(currentPage.value)
+    .then((res) => {
+      tableData.value = res.list
+      total.value = res.totalCount
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
+
+getData()
+
+const formDrawerRef = ref(null)
+const formRef = ref(null)
+const form = reactive({
+  title: '',
+  content: ''
+})
+const rules = {
+  title: [],
+  content: []
+}
+const editId = ref(0)
+const drawerTitle = computed(() => (editId.value ? '修改' : '新增'))
+
+const handleSubmit = () => {
+  formRef.value.validate((valid) => {
+    if (!valid) return
+    formDrawerRef.value.showLoading()
+
+    const fun = editId.value
+      ? updateNoticeList(editId.value, form)
+      : addNoticeList(form)
+
+    fun.then((res) => {
+        toast(drawerTitle.value + '成功!')
+        // 修改刷新当前页，新增刷新第一页
+        getData(editId.value ? false : 1)
+        formDrawerRef.value.close()
+      })
+      .finally(() => {
+        formDrawerRef.value.hideLoading()
+      })
+  })
+}
+
+// 重置表单
+function resetForm(row = false) {
+  if (formRef.value) formRef.value.clearValidate()
+  if (row) {
+    for (const key in form) {
+      form[key] = row[key]
+    }
+  }
+}
+
+// 新增
+const handleCreate = () => {
+  editId.value = 0
+  resetForm({
+    title: '',
+    content: ''
+  })
+  formDrawerRef.value.open()
+}
+
+// 修改
+const handleEdit = (row) => {
+  editId.value = row.id
+  resetForm(row)
+  formDrawerRef.value.open()
+}
+
+// 删除
+const handleDelete = (id) => {
+  loading.value = true
+  deleteNoticeList(id)
+    .then((res) => {
+      toast('删除成功！')
+      getData()
+      console.log(res)
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
+</script>
