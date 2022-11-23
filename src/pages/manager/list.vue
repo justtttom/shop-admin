9842bf5ebaf15e1a5bfead037d79314fc5ba0352
Justin Tom <2541198816@qq.com>
2022-11-23 20:10:13 +0800

    add rules about manager.page

--- src/pages/manager/list.vue
+++ src/pages/manager/list.vue
@@ -208,11 +208,20 @@ const form = reactive({
   username: '',
   password: '',
   role_id:null,
-  
+  status:1,
+  avatar:""
 })
 const rules = {
-  title: [],
-  content: []
+  username:[{
+      required: true,
+      message: '用户名不能为空',
+      trigger: 'blur'
+  }],
+  password:[{
+      required: true,
+      message: '密码不能为空',
+      trigger: 'blur'
+  }]
 }
 const editId = ref(0)
 const drawerTitle = computed(() => (editId.value ? '修改' : '新增'))
