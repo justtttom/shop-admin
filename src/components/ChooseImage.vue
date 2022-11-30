f2c96e4f19113840fe88c32ea5154a04f4cb9401
Justin Tom <2541198816@qq.com>
2022-11-30 21:15:05 +0800

    import Image unit in ChooseImage.vue

--- src/components/ChooseImage.vue
+++ src/components/ChooseImage.vue
@@ -1,25 +1,49 @@
 <template>
-  <div class="choose-image-btn">
+  <div class="choose-image-btn" @click="open">
     <el-icon :size="25" class="text-green-400"><Plus /></el-icon>
   </div>
   <el-dialog
     title="选择图片"
     v-model="dialogVisible"
     width="80%"
+    top="5vh"
   >
-    <span></span>
+  <el-container class="bg-white rounded" :style="{ height: h + 'px' }">
+    <el-header class="image-header">
+      <el-button type="primary" size="small" @click="handleOpenCreate">
+        新增图片分类
+      </el-button>
+      <el-button type="warning" size="small" @click="handleOpenUpload">
+        上传图片
+      </el-button>
+    </el-header>
+    <el-container>
+      <ImageAside ref="ImageAsideRef" @change="handleAsideChange"/>
+      <ImageMain ref="imageMainRef"/>
+    </el-container>
+  </el-container>
     <template #footer>
       <span>
-        <el-button @click=" false">Cancel</el-button>
-        <el-button type="primary" @click="">OK</el-button>
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="submit">OK</el-button>
       </span>
     </template>
   </el-dialog>
 </template>
 <script setup>
 import { ref } from 'vue'
+import ImageAside from './ImageAside.vue';
+import ImageMain from './ImageAside.vue';
 
 const dialogVisible = ref(false)
+
+const open = ()=>{
+  dialogVisible.value = true
+}
+
+const submit = ()=>{
+
+}
 </script>
 
 <style>
