0000911dfe25708e0c493864a027dced7b0fad4d
Justin Tom <2541198816@qq.com>
2022-12-05 20:27:24 +0800

    correction the error of the submit

--- src/components/ChooseImage.vue
+++ src/components/ChooseImage.vue
@@ -2,29 +2,24 @@
   <div class="choose-image-btn" @click="open">
     <el-icon :size="25" class="text-green-400"><Plus /></el-icon>
   </div>
-  <el-dialog
-    title="选择图片"
-    v-model="dialogVisible"
-    width="80%"
-    top="5vh"
-  >
-  <el-container class="bg-white rounded" style="height:70vh;">
-    <el-header class="image-header">
-      <el-button type="primary" size="small" @click="handleOpenCreate">
-        新增图片分类
-      </el-button>
-      <el-button type="warning" size="small" @click="handleOpenUpload">
-        上传图片
-      </el-button>
-    </el-header>
-    <el-container>
-      <ImageAside ref="ImageAsideRef" @change="handleAsideChange"/>
-      <ImageMain ref="imageMainRef" @choose="handleChoose"/>
+  <el-dialog title="选择图片" v-model="dialogVisible" width="80%" top="5vh">
+    <el-container class="bg-white rounded" style="height: 70vh;">
+      <el-header class="image-header">
+        <el-button type="primary" size="small" @click="handleOpenCreate">
+          新增图片分类
+        </el-button>
+        <el-button type="warning" size="small" @click="handleOpenUpload">
+          上传图片
+        </el-button>
+      </el-header>
+      <el-container>
+        <ImageAside ref="ImageAsideRef" @change="handleAsideChange" />
+        <ImageMain ref="imageMainRef" @choose="handleChoose" />
+      </el-container>
     </el-container>
-  </el-container>
     <template #footer>
       <span>
-        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button @click="close">取消</el-button>
         <el-button type="primary" @click="submit">确认</el-button>
       </span>
     </template>
@@ -32,46 +27,45 @@
 </template>
 <script setup>
 import { ref } from 'vue'
-import ImageAside from './ImageAside.vue';
-import ImageMain from './ImageMain.vue';
-
+import ImageAside from './ImageAside.vue'
+import ImageMain from './ImageMain.vue'
 
 const dialogVisible = ref(false)
 
-const open = ()=>{
-  dialogVisible.value = true
-}
+const open = () => (dialogVisible.value = true)
 
-
+const close = () => (dialogVisible.value = false)
 
 const ImageAsideRef = ref(null)
 
-const handleOpenCreate = ()=>{
+const handleOpenCreate = () => {
   ImageAsideRef.value.handleCreate()
 }
 
 const imageMainRef = ref(null)
-const handleAsideChange = (image_class_id)=>{
+const handleAsideChange = (image_class_id) => {
   imageMainRef.value.loadData(image_class_id)
 }
 
-const handleOpenUpload = ()=>{
+const handleOpenUpload = () => {
   imageMainRef.value.openUploadFile()
 }
 
-const props =  defineProps({
-  modelValue:[String,Array]
+const props = defineProps({
+  modelValue: [String, Array]
 })
-const emit = defineEmits(["update:modelValue"])
-const handleChoose = (e)=>{
-  let urls = []
-  urls = e.map(o=>o.url)
+const emit = defineEmits(['update:modelValue'])
+
+let urls = []
+const handleChoose = (e) => {
+  urls = e.map((o) => o.url)
 }
 
-const submit = ()=>{
-  if(urls.length){
-    emit("update:modelValue",urls[0])
+const submit = () => {
+  if (urls.length) {
+    emit('update:modelValue', urls[0])
   }
+  close()
 }
 </script>
 
