9040cd63023e867cee47ee1afb5a8b10a953bb6a
Justin Tom <2541198816@qq.com>
2022-12-05 19:52:38 +0800

    monday working

--- src/pages/manager/list.vue
+++ src/pages/manager/list.vue
@@ -127,7 +127,7 @@
           <el-input v-model="form.password" placeholder="请输入密码"></el-input>
         </el-form-item>
         <el-form-item label="头像" prop="avatar">
-            <ChooseImage/>
+            <ChooseImage v-model="form.avatar"/>
         </el-form-item>
         <el-form-item label="所属角色" prop="role_id">
           <el-select
