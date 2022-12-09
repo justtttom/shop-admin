7f649c475ac573644160b772aaa409d7e56b9010
Justin Tom <2541198816@qq.com>
2022-12-09 21:51:12 +0800

    import ListHeader to access.page

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -1,18 +1,10 @@
 <template>
   <el-card shadow="never" class="border-0">
       <!-- 新增 刷新 -->
-      <div class="flex justify-between items-center mb-4">
-      <el-button type="success" size="small" @click=""
-        >新增</el-button
-      >
-      <el-tooltip effect="dark" content="刷新数据" placement="top">
-        <el-button text @click="">
-          <el-icon :size="20">
-            <Refresh />
-          </el-icon>
-        </el-button>
-      </el-tooltip>
-    </div>
+      <ListHeader/>
   </el-card>
-  
 </template>
+
+<script setup >
+  import ListHeader from '~/components/ListHeader.vue'
+</script>
