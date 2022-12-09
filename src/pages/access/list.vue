ece3ab643e76ce7a9cc6c5e34bdd64152d382513
Justin Tom <2541198816@qq.com>
2022-12-09 20:45:15 +0800

    access.page change

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    权限管理页面
-  </div>
+  <el-card shadow="never" class="border-0">
+      <!-- 新增 刷新 -->
+      <div class="flex justify-between items-center mb-4">
+      <el-button type="success" size="small" @click=""
+        >新增</el-button
+      >
+      <el-tooltip effect="dark" content="刷新数据" placement="top">
+        <el-button text @click="">
+          <el-icon :size="20">
+            <Refresh />
+          </el-icon>
+        </el-button>
+      </el-tooltip>
+    </div>
+  </el-card>
+  
 </template>
