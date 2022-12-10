ce6c5ed5fff97561ded7fd3da31baaa2e271655b
Justin Tom <2541198816@qq.com>
2022-12-10 22:25:22 +0800

    import useCommon to access.page

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -1,7 +1,7 @@
 <template>
   <el-card shadow="never" class="border-0">
       <!-- 新增 刷新 -->
-      <ListHeader/>
+      <ListHeader @refresh="getData"/>
       <!-- <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" /> -->
   </el-card>
 </template>
@@ -10,4 +10,12 @@
   import ListHeader from '~/components/ListHeader.vue'
   import {getRuleList} from '~/api/rule.js'
   import {useInitTable} from '~/composables/useCommon'
+
+  const {
+    loading,
+    tableData,
+    getData,
+  } = useInitTable({
+    getList:getRuleList,
+  })
 </script>
