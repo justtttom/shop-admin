6a1ad808ba19d6dff08db71829408a0416a2db54
Justin Tom <2541198816@qq.com>
2022-12-12 20:54:04 +0800

    onGetListSuccess function change

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -1,21 +1,28 @@
 <template>
   <el-card shadow="never" class="border-0">
-      <!-- 新增 刷新 -->
-      <ListHeader @refresh="getData"/>
-      <el-tree :data="tableData" :props="{label:'name',children:'child'}"/>
+    <!-- 新增 刷新 -->
+    <ListHeader @refresh="getData" />
+    <el-tree
+      :data="tableData"
+      :props="{ label: 'name', children: 'child' }"
+      v-loading="loading"
+      node-key="id"
+      :default-checked-keys="defaultExpandedkeys"
+    />
   </el-card>
 </template>
 
-<script setup >
-  import ListHeader from '~/components/ListHeader.vue'
-  import {getRuleList} from '~/api/rule.js'
-  import {useInitTable} from '~/composables/useCommon'
+<script setup>
+import { ref } from 'vue'
+import ListHeader from '~/components/ListHeader.vue'
+import { getRuleList } from '~/api/rule.js'
+import { useInitTable } from '~/composables/useCommon'
 
-  const {
-    loading,
-    tableData,
-    getData,
-  } = useInitTable({
-    getlist:getRuleList,
-  })
+const defaultExpandedkeys = ref([])
+const { loading, tableData, getData } = useInitTable({
+  getlist: getRuleList,
+  onGetListSuccess:(res)=>{
+    console.log(res);
+  }
+})
 </script>
