bcc8b4119de7b20796b63f7ef4ebeb0b75ec065d
Justin Tom <2541198816@qq.com>
2022-12-13 21:05:17 +0800

    create addRule and updateRule api

--- src/api/rule.js
+++ src/api/rule.js
@@ -3,3 +3,11 @@ import axios from '~/axios'
 export function getRuleList(page) {
   return axios.get(`/admin/rule/${page}`)
 }
+
+export function addRule(data){
+  return axios.post(`/admin/notice`,data)
+}
+
+export function updateRule(id,data){
+  return axios.post(`/admin/notice/${id}`,data)
+}
