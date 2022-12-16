62d3550f44433682a836723d311ea3132815ff8b
Justin Tom <2541198816@qq.com>
2022-12-16 20:13:14 +0800

    create role api list

---
+++ src/api/role.js
@@ -0,0 +1,28 @@
+import axios from '~/axios'
+
+export function getRoleList(page) {
+  return axios.get(`/admin/role/${page}`)
+}
+
+export function addRole(data){
+  return axios.post(`/admin/role`,data)
+}
+
+export function updateRole(id,data){
+  return axios.post(`/admin/role/${id}`,data)
+}
+
+
+export function deleteRole(id){
+  return axios.post(`/admin/role/${id}/delete`)
+}
+
+export function updateRoleStatus(id, status) {
+  return axios.post(`/admin/role/${id}/update_status`, {
+    status
+  })
+}
+
+export function deleteRole(id){
+  return axios.post(`/dmin/role/set_rules`,id)
+}
