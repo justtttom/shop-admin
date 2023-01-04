fdca78ae31575b92130bc346ee2010cce8ff7b90
Justin Tom <2541198816@qq.com>
2023-01-04 22:13:55 +0800

    coupon name change about coupon.page

--- src/pages/coupon/list.vue
+++ src/pages/coupon/list.vue
@@ -3,7 +3,14 @@
     <!-- 新增|刷新 -->
     <ListHeader @create="handleCreate" @refresh="getData" />
     <el-table :data="tableData" stripe style="width: 100%;" v-loading="loading">
-      <el-table-column prop="name" label="优惠券名称" width="350"/>
+      <el-table-column label="优惠券名称" width="350">
+      <template #default="{row}">
+        <div class="border bortder-dashed py-2 px-4 rounded">
+        <h5 class="text-gray-500">{{row.name}}</h5>
+        <small>{{row.start_time}}~{{row.end_time}}</small>
+        </div>
+      </template>
+      </el-table-column>
       <el-table-column prop="type" label="状态"/> 
       <el-table-column prop="value" label="优惠" />
       <el-table-column prop="total" label="发放数量" />
