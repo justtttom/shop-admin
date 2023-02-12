50656957948c66eb41855bbcbc16f58efa3a836e
justtttom <2541198816@qq.com>
2023-02-12 15:12:58 +0800

    01

--- src/pages/coupon/list.vue
+++ src/pages/coupon/list.vue
@@ -5,10 +5,7 @@
     <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
       <el-table-column label="优惠券名称" width="350">
         <template #default="{ row }">
-          <div
-            class="border bortder-dashed py-2 px-4 rounded"
-            :class="row.statusText == '领取中' ? 'active' : 'inactive'"
-          >
+          <div class="border bortder-dashed py-2 px-4 rounded" :class="row.statusText == '领取中' ? 'active' : 'inactive'">
             <h5 class="font-bold text-md">{{ row.name }}</h5>
             <small> {{ row.start_time }} ~ {{ row.end_time }} </small>
           </div>
@@ -25,16 +22,9 @@
       <el-table-column prop="used" label="已使用" />
       <el-table-column label="操作" align="center">
         <template #default="scope">
-          <el-button size="small" type="primary" @click="handleEdit(scope.row)" text
-            >修改</el-button
-          >
-          <el-popconfirm
-            title="是否要删除该优惠？"
-            confirm-button-text="确定"
-            cancel-button-text="取消"
-            width="20"
-            @confirm="handleDelete(scope.row.id)"
-          >
+          <el-button size="small" type="primary" @click="handleEdit(scope.row)" text>修改</el-button>
+          <el-popconfirm title="是否要删除该优惠？" confirm-button-text="确定" cancel-button-text="取消" width="20"
+            @confirm="handleDelete(scope.row.id)">
             <template #reference>
               <el-button size="small" type="primary" text>删除</el-button>
             </template>
@@ -43,23 +33,11 @@
       </el-table-column>
     </el-table>
     <div class="flex items-center justify-center mt-5">
-      <el-pagination
-        background
-        layout="prev, pager,next"
-        :total="total"
-        :current-page="currentPage"
-        :page-size="limit"
-        @current-change="getData"
-      />
+      <el-pagination background layout="prev, pager,next" :total="total" :current-page="currentPage" :page-size="limit"
+        @current-change="getData" />
     </div>
     <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
-      <el-form
-        :model="form"
-        ref="formRef"
-        :rules="rules"
-        label-width="80px"
-        :inline="false"
-      >
+      <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
         <el-form-item label="优惠券名称" prop="name" style="width: 60%">
           <el-input v-model="form.name" placeholder="优惠券名称"></el-input>
         </el-form-item>
@@ -84,14 +62,8 @@
           <el-input-number v-model="form.order" :min="0" :max="1000"></el-input-number>
         </el-form-item>
         <el-form-item label="活动时间" :rows="5">
-          <el-date-picker
-            v-model="timerange"
-            type="datetimerange"
-            value-format="YYYY-MM-DD HH:mm:ss"
-            range-separator="To"
-            start-placeholder="开始时间 date"
-            end-placeholder="结束时间"
-          />
+          <el-date-picker v-model="timerange" type="datetimerange" value-format="YYYY-MM-DD HH:mm:ss"
+            range-separator="To" start-placeholder="开始时间 date" end-placeholder="结束时间" />
         </el-form-item>
         <el-form-item label="描述" prop="desc" :rows="5">
           <el-input v-model="form.desc" placeholder="描述"></el-input>
@@ -189,11 +161,11 @@ const {
   getData,
   update: updateCoupon,
   add: addCoupon,
-  beforeSbumit:(f)=>{
-    if(typeof f.start_time != 'number'){
+  beforeSbumit: (f) => {
+    if (typeof f.start_time != 'number') {
       f.start_time = (new Date(f.start_time)).getTime
     }
-    if(typeof f.end_time != 'number'){
+    if (typeof f.end_time != 'number') {
       f.end_time = (new Date(f.end_time)).getTime
     }
     return f
@@ -204,7 +176,7 @@ const timerange = computed({
   get() {
     return form.start_time, form.end_time ? [form.start_time, form.end_time] : [];
   },
-  set(val){
+  set(val) {
     form.start_time = val[0]
     form.end_time = val[1]
   }
