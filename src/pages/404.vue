791f05e0f249d917b4d1c502ecf7dbfe431eb32a
justtttom <2541198816@qq.com>
2023-05-21 10:37:02 +0800

    keep learing

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <el-result icon="warning" title="404提示" sub-title="你的页面走丢了">
+    <el-result icon="warning" title="404提示" sub-title="页面不存在！！！">
       <template #extra>
-        <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
+        <el-button type="primary" @click="$router.push('/')">请返回首页</el-button>
       </template>
     </el-result>
   </div>
